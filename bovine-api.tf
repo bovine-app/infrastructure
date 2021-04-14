44f31b25cc0e9a10948a8ea7f3a8ac6ded7e3b21
Jesse B. Hannah <jesse@jbhannah.net>
2021-04-14 15:52:10 -0700

    Create API repository

---
+++ bovine-api.tf
@@ -0,0 +1,25 @@
+resource "github_repository" "api" {
+  name        = "bovine-api"
+  description = "API server for the Bovine podcatcher"
+
+  gitignore_template = "Rails"
+  license_template   = "mit"
+
+  auto_init              = true
+  delete_branch_on_merge = true
+}
+
+resource "github_branch_protection" "api_default_branch" {
+  repository_id = github_repository.api.node_id
+  pattern       = github_repository.api.default_branch
+
+  allows_deletions    = false
+  allows_force_pushes = false
+  enforce_admins      = true
+
+  required_pull_request_reviews {
+    dismiss_stale_reviews           = true
+    require_code_owner_reviews      = true
+    required_approving_review_count = 1
+  }
+}
