da32d1d0bb198acb99dbe0b21cbcbc740faf1597
Jesse B. Hannah <jesse@jbhannah.net>
2021-04-14 17:33:02 -0700

    Add Terraform Cloud

--- main.tf
+++ main.tf
@@ -4,10 +4,27 @@ terraform {
       source  = "integrations/github"
       version = "4.8.0"
     }
+    tfe = {
+      source  = "hashicorp/tfe"
+      version = "0.24.0"
+    }
+  }
+
+  backend "remote" {
+    hostname     = "app.terraform.io"
+    organization = "bovine-app"
+
+    workspaces {
+      name = "infrastructure"
+    }
   }
 }
 
 provider "github" {
   owner = "bovine-app"
   token = var.github_token
+}
+
+provider "tfe" {
+  token = var.tfe_token
 }
