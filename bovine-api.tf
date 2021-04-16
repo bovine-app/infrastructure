889ffe1e0d8aee52a8fea528a849bb197a718aa4
Jesse B. Hannah <jesse@jbhannah.net>
2021-04-15 18:39:56 -0700

    Remove approving review requirements

--- bovine-api.tf
+++ bovine-api.tf
@@ -20,9 +20,8 @@ resource "github_branch_protection" "api_default_branch" {
   enforce_admins      = false
 
   required_pull_request_reviews {
-    dismiss_stale_reviews           = true
-    require_code_owner_reviews      = true
-    required_approving_review_count = 1
+    dismiss_stale_reviews      = true
+    require_code_owner_reviews = true
   }
 }
 
--- bovine-web.tf
+++ bovine-web.tf
@@ -20,8 +20,7 @@ resource "github_branch_protection" "web_default_branch" {
   enforce_admins      = false
 
   required_pull_request_reviews {
-    dismiss_stale_reviews           = true
-    require_code_owner_reviews      = true
-    required_approving_review_count = 1
+    dismiss_stale_reviews      = true
+    require_code_owner_reviews = true
   }
 }
