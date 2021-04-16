8cefa80f0af0ffc486e5549999b7bf7916e8fbf0
Jesse B. Hannah <jesse@jbhannah.net>
2021-04-15 18:41:02 -0700

    Set approving review count requirement to 0

--- infrastructure.tf
+++ infrastructure.tf
@@ -20,7 +20,8 @@ resource "github_branch_protection" "infra_default_branch" {
   enforce_admins      = false
 
   required_pull_request_reviews {
-    dismiss_stale_reviews      = true
-    require_code_owner_reviews = true
+    dismiss_stale_reviews           = true
+    require_code_owner_reviews      = true
+    required_approving_review_count = 0
   }
 }
